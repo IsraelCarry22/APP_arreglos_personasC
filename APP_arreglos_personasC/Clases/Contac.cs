3cddf33ff7bf417deb2b066cbeecd73aba9d12ab
Israel Carreon <Israelcarreon992@gmail.com>
2023-02-25 09:32:27 -0600

    Agregar archivos de proyecto.

---
+++ APP_arreglos_personasC/Clases/Contac.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp_16_02_23;
+
+namespace WindowsFormsApp_16_02_23
+{
+    class Contac : Person
+    {
+        private String telephone;
+        private String gmail;
+
+        public string Telefono
+        {
+            get { return telephone; }
+            set
+            {
+                telephone = value.Replace(" ", "").Replace("-", "");
+            }
+        } 
+
+        public String Correo
+        {
+            get { return gmail; }
+            set { gmail = value; }
+        }
+
+        public Contac() : base()
+        {
+            telephone = string.Empty;
+            gmail = string.Empty;
+        }
+
+        public Contac(string name, string last_name_father, string last_name_mother, DateTime? birthdate, string telephone, string gmail) : base(name, last_name_father, last_name_mother, birthdate)
+        {
+            this.telephone = telephone;
+            this.gmail= gmail;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\nTelefono: " + telephone + "\nCorreo: " + gmail;
+        }
+    }
+}
---
+++ APP_arreglos_personasC/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp_16_02_23;
+
+namespace APP_arreglos_personasC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero_contactos = 0;
+            Console.WriteLine("=====AGENDA DE PERSONAS=====\n");
+            Console.Write("Ingrese la cantidad de contactos que quiere agregar: ");
+            numero_contactos = int.Parse(Console.ReadLine());
+            Contac[] Personas = new Contac[numero_contactos];
+            Console.Clear();
+            for (int i = 0; i < numero_contactos; i++)
+            {
+                Console.WriteLine("=====CONTACTO =====\n");
+                Contac Agenda = new Contac();
+                Console.Write("Ingresa el Nombre: ");
+                Agenda.Nombre = Console.ReadLine();
+                Console.Write("Ingresa tu Apellido paterno: ");
+                Agenda.Apellido_Paterno = Console.ReadLine();
+                Console.Write("Ingresa el Apellido materno: ");
+                Agenda.Apellido_Materno = Console.ReadLine();
+                Console.Write("Ingresa tu Fecha de cumpleaños (DD/MM/AAAA): ");
+                Agenda.Fecha_de_cumpleaños = DateTime.Parse(Console.ReadLine());
+                Console.Write("Ingresa tu Correo: ");
+                Agenda.Correo = Console.ReadLine();
+                Console.Write("Ingresa tu Telefono: ");
+                Agenda.Telefono = Console.ReadLine();
+                Personas[i] = Agenda;
+                Console.Clear();
+            }
+
+            for (int i = 0; i < numero_contactos; i++)
+            {
+                Console.WriteLine("Contacto #"+(i+1));
+                Console.WriteLine(Personas[i].ToString() + "\n");
+            }
+            Console.WriteLine("Pulse entre para salir...");
+            Console.ReadKey();
+        }
+    }
+}
